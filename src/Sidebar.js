d7d6baf83e95109fe2233b4e3eee9301f1391bff
Luca-Al-Kafiri <lucaalkafiri@gmail.com>
2020-12-20 02:21:46 +0400

    react twelfth project

---
+++ src/Sidebar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import logo from "./logo.svg";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { social, links } from "./data";
+import { useGlobalContext } from "./context";
+
+const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"}`}>
+      <div className="sidebar-header">
+        <img src={logo} className="logo" alt="logo" />
+        <button onClick={closeSidebar} className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, icon, text, url } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-icons">
+        {social.map((link) => {
+          const { id, icon, url } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
